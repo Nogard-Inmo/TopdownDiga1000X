2506afed7266d95dcd40bb4a977446840b0f7994
Nogard-Inmo <liam.nordlund09@gmail.com>
2025-11-24 14:59:30 +0100

    start on added money manager

---
+++ Assets/Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    float rebirths = 0f;
+
+    float rebirthMultiplier = 1f + (rebirths * 1.1f);
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
